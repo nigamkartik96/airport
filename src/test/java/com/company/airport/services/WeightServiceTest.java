826b25d8ff9e9fff072b07e810402ebe34f6b23c
nigamkartik <nigamkartik962@gmail.com>
2022-07-17 17:58:19 +0530

    master: Weight Service Test

---
+++ src/test/java/com/company/airport/services/WeightServiceTest.java
@@ -0,0 +1,60 @@
+package com.company.airport.services;
+
+import com.company.airport.dao.FlightDao;
+import com.company.airport.dao.WeightDAO;
+import com.company.airport.models.flightapi.FlightDetails;
+import com.company.airport.models.weightapi.FlightWeight;
+import com.company.airport.models.weightapi.WeightResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+
+public class WeightServiceTest {
+    @Mock
+    private transient FlightDao flightDao;
+    @Mock
+    private transient WeightDAO weightDAO;
+
+    private WeightService weightService;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+        weightService = new WeightService(flightDao, weightDAO);
+    }
+
+    private FlightWeight getFlightDetails() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            File file = ResourceUtils.getFile("classpath:weight_response.json");
+            return objectMapper.treeToValue(objectMapper.readTree(file), FlightWeight.class);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    @Test
+    public void testGetWeightResponse_noError() {
+        Mockito.doReturn(1).when(flightDao).getFlightIdFromFlightNumber(ArgumentMatchers.anyString());
+        Mockito.doReturn(getFlightDetails()).when(weightDAO).getFlightWeightDetails(ArgumentMatchers.anyInt());
+
+        WeightResponse weightResponse = weightService.getWeightResponse("123", "123");
+        assert weightResponse != null;
+    }
+
+    @Test
+    public void testGetWeightResponse_flightNotFound() {
+        Mockito.doReturn(-1).when(flightDao).getFlightIdFromFlightNumber(ArgumentMatchers.anyString());
+
+        WeightResponse weightResponse = weightService.getWeightResponse("123", "123");
+        System.out.println(weightResponse);
+        assert "Flight with FlightNumber=123 not present, please check again.".equals(weightResponse.getError());
+    }
+}
